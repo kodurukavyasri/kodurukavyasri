1c294ee83510ff67832ced11f5b36292f34de05b
kodurukavyasri <142497454+kodurukavyasri@users.noreply.github.com>
2023-11-11 17:43:54 +0530

    Create CalculatePercentageOf5Subjects.c

---
+++ CalculatePercentageOf5Subjects.c
@@ -0,0 +1,17 @@
+/*
+Problem Statement
+It is simple to calculate the percentage of five subjects, all you need to do is add all of the marks and multiply that total by 100. 
+Then divide that by the total number of marks a student is permitted to receive. 
+You will ultimately receive the results as a percentage of the 5 subjects.
+
+Input:
+50 20 30 40 80
+
+Output:
+Enter marks of five subjects:
+Total marks = 220.00
+Average marks = 44.00
+Percentage = 44.00
+*/
+
+// Start your code here
