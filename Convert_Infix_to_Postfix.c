8e4544f6721e3183b21a6c236bf22ce9e32df610
kodurukavyasri <142497454+kodurukavyasri@users.noreply.github.com>
2023-11-11 17:46:35 +0530

    Create Convert_Infix_to_Postfix.c

---
+++ Convert_Infix_to_Postfix.c
@@ -0,0 +1,13 @@
+/*
+Problem Statement
+Any operation may be written as an infix, prefix, or postfix, in this C language tutorial, we’ll see how to do this. 
+Two approaches have been covered: the array-based stack approach and the struct-based stack approach.
+
+Input:
+((p+(q*r))-s)
+
+Output:
+pqr*+s-
+*/
+
+// Start your code here
