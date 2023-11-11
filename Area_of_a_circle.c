bf8fe5d2ad3544214d107d64b9db17526d91de3d
kodurukavyasri <142497454+kodurukavyasri@users.noreply.github.com>
2023-11-11 17:56:59 +0530

    Add files via upload

---
+++ Area_of_a_circle.c
@@ -0,0 +1,17 @@
+/*
+Problem Statement
+Everyone knows the basics of mathematics,
+For finding the area of a circle, we can get this by performing some
+Print area with 2 decimals only
+
+Area = Pie * Radius * Radius
+Where the value of pie = 3.14 and the radius will be given by the user.
+
+Input:
+10
+
+Output:
+The radius of the circle is 10
+The area of a given circle is 314.00
+
+// Start your code here
---
+++ Convert_Celsius_to_Fahrenheit.c
@@ -0,0 +1,16 @@
+/*
+Problem Statement
+Convert_Celsius_to_Fahrenheit
+In the realm of programming, we may easily convert between celsius and fahrenheit by carrying out a few simple mathematical operations.
+The formula to change from celsius to fahrenheit is shown below.
+
+Formula
+Fahrenheit Temperature = (1.8 * celsius) + 32;
+
+Input:
+40
+
+Output:
+104
+
+// Start your code here
