6ab41696bc54d7f1301968129aeae68d79008574
kodurukavyasri <142497454+kodurukavyasri@users.noreply.github.com>
2023-11-11 17:48:05 +0530

    Create Odd_Even.c

---
+++ Odd_Even.c
@@ -0,0 +1,21 @@
+/*
+Problem Statement
+A number is taken into consideration even though it may be split by two equally. 
+The remainder that is not precisely divisible by 2 is known as an odd number. 
+Simply expressed, odd numbers adopt the form n = 2k+1, whereas even numbers take the form n = 2k. 
+Each integer will be made up of either even or odd integers.
+
+Input:
+10
+
+Output:
+Even
+
+Input:
+15
+
+Output:
+Odd
+*/
+
+// Start your code here
